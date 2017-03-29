220209861304fd81d2c22218429b213c36aaec41
Varsha Pathak <varsha.pathak@sjsu.edu>
2017-03-29 02:07:23 -0700

    Hello World Program

---
+++ hello_world.go
@@ -0,0 +1,10 @@
+// Ever go program starts with a package declaration.
+package main
+
+// Package for stdio
+import "fmt"
+
+// Calling main function
+func main() {
+	fmt.Println("Hello World");
+}
